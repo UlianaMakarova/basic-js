0d9cbd8491360f903ecbecfe06f6ae40550ad7de
ulianamakarovaulianamakarovaexitgit config --globalgit config --global user.name ulianamakarova <ulianamakarova@gmail.com>
2023-04-17 22:33:50 +0300

    feat: add transform-array.js

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,57 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
+function transform(arr) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  
+  if (!Array.isArray(arr))
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  result = []
+  discard = false;
+  arr.forEach((el,index)=>{
+    switch (el){
+      case '--discard-next':
+      {
+       
+        discard = true;
+        break;
+      }  
+      case '--discard-prev':
+        {
+          result.pop();
+          break;
+        }
+      case '--double-next':
+        {
+          if (typeof arr[index + 1] === 'number')
+          {
+            next = arr[index+1];
+            result.push(next);
+          }
+          break;
+        }
+      case '--double-prev':
+        {
+          if (typeof arr[index - 1] === 'number')
+          {
+            prev = arr[index-1];
+            result.push(prev);
+          }
+          break;
+        }
+        default:{
+          if (discard)
+          {
+            arr.splice(index,1);
+            discard = false;
+          }
+          else{
+            result.push(el);
+          }
+        }
+  }})
+  return result
 }
 
 module.exports = {

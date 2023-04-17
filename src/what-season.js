635b3296b649ff14a74c1cf3a5ddf67e4fe0849b
ulianamakarovaulianamakarovaexitgit config --globalgit config --global user.name ulianamakarova <ulianamakarova@gmail.com>
2023-04-17 20:57:56 +0300

    feat: add what season

--- src/what-season.js
+++ src/what-season.js
@@ -11,10 +11,29 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
+function getSeason(date) {
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
-}
+  if (typeof date === 'undefined')
+  return "Unable to determine the time of year!"
+  if (Object.prototype.toString.call(date) !== '[object Date]' ||  date==Date.prototype.toString.call(new Date()))
+  return "Invalid date!"
+  winter = [12,1,2]
+  spring = [3,4,5]
+  summer = [6,7,8]
+  autumn = [9,10,11]
+  month = date.getMonth() === 12? 1: date.getMonth()+1
+  if (winter.indexOf(month) != -1)
+    return "winter"
+  else if (spring.indexOf(month) != -1)  
+    return "spring"
+  else if (summer.indexOf(month) != -1)
+    return "summer"
+  else if (autumn.indexOf(month) != -1)
+    return "autumn"
+  else 
+  return "Invalid date!"
+  }
 
 module.exports = {
   getSeason
